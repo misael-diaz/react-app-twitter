d895df112967b3519248343cb7702e2442e2d2bc
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-03-27 09:36:33 -0400

    validates emails, usernames, passwords, etc. [JSX]
    COMMENTS:
    Adds code to validate Names, Emails, Usernames, and Passwords in
    the SignUp Page.
    
    Adds used references for the next programmer.
    
    The code has been added to get an idea of how regular expressions
    could be used to validate user input in a React App.
    
    Note that improved versions must be developed for a production
    environment, especially the imposed email restrictions could affect
    negatively the user experience UX.

--- twitter/src/components/public/signup/index.jsx
+++ twitter/src/components/public/signup/index.jsx
@@ -38,6 +38,59 @@ const Signup = () => {
 
   }, []);
 
+  const hasWhitespace = (str) => {
+
+    const regex = /\s/;
+    const match = (str.match(regex) || []);
+    const spaces = match.length;
+    return (spaces !== 0);
+
+  };
+
+  const isInvalidName = (str) => {
+
+    const regex = /([A-Z][a-z]+)/;
+    const match = (str.match(regex) || [[]]);
+    return (match[0].length !== str.length);
+
+  };
+
+  const isInvalidPassword = (str) => {
+
+    // eslint-disable-next-line
+    const regex = /([-\.\w]+)/;
+    const match = (str.match(regex) || [[]]);
+    return (match[0].length !== str.length);
+
+  };
+
+  const isWeakPassword = (str) => {
+    // checks for minimum password requirements
+
+    const numbers = (str.match(/(?:[0-9])/g) || []).length;
+    const symbols = (str.match(/(?:[-\._])/g) || []).length;	// eslint-disable-line
+    const capital = (str.match(/(?:[A-Z])/g) || []).length;
+
+    return (numbers === 0 || symbols === 0 || capital === 0)? true : false;
+
+  };
+
+  const isInvalidEmail = (str) => {
+    // Returns true if the email does not have the form:
+    //
+    // 		firstName[.lastName][0-9]*@emailProvider.com[.country]
+    //
+    // where the .lastName, the digits, and the .country are optional sub-strings.
+    // Note: see reference [5] for a more general regular expression
+
+    const email = str.toLowerCase();
+    // eslint-disable-next-line
+    const re = /([a-z]+([-\._][a-z]+){0,1}[0-9]*@([a-z]+\.)+[a-z]{3}(\.[a-z]{2}){0,1})/;
+    const match = (email.match(re) || [[]]);
+    return (match[0].length !== str.length);
+
+  };
+
   const handleClick = () => {
 
     for (const prop in user)
@@ -62,6 +115,64 @@ const Signup = () => {
 
       }
 
+      if (prop !== "confirmation" && prop !== "login")
+      {
+
+	if ( hasWhitespace(user[prop]) )
+	{
+	  const cls = "error";
+	  const msg = `${prop} has whitespace`;
+	  setMessage({ class: cls, content: msg });
+	  return;
+	}
+
+      }
+
+    }
+
+    if (isInvalidName(user.name))
+    {
+      const cls = "error";
+      const msg = "Name is not capitalized or has numbers and/or special characters";
+      setMessage({ class: cls, content: msg });
+      return;
+    }
+
+    if (isInvalidEmail(user.email))
+    {
+      const cls = "error";
+      const msg = "invalid email, consider the following valid examples: " +
+	"firstName@gmail.com, firstName17@gmail.com, firstName.lastName@gmail.com, " +
+	"firstName-lastName@gmail.com, firstName_lastName@gmail.com, " +
+	"firstName.lastName17@gmail.com, firstName.lastName@university.edu.co";
+      setMessage({ class: cls, content: msg });
+      return;
+    }
+
+    if (isInvalidPassword(user.password))
+    {
+      const cls = "error";
+      const msg = "passwords can only contain alpha numeric characters, " +
+	"underscores (_), periods (.), and hyphens (-)";
+      setMessage({ class: cls, content: msg });
+      return;
+    }
+
+    if (isWeakPassword(user.password))
+    {
+      const cls = "error";
+      const msg = "passwords must contain capital letters, numbers, and " +
+	"at least a period (.), an underscore (_), or a hyphen (-)";
+      setMessage({ class: cls, content: msg });
+      return;
+    }
+
+    if (user.password.length < 8 || user.password.length > 16)
+    {
+      const cls = "error";
+      const msg = "passwords must be at least 8 and at most 16 characters long"
+      setMessage({ class: cls, content: msg });
+      return;
     }
 
     const { password, confirmation } = user;
@@ -230,5 +341,8 @@ References:
 [0] https://github.com/jestrade/cec-twitter
 [1] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
 [2] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON
+[3] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+[4] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#advanced_searching_with_flags
+[5] https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
 
 */
